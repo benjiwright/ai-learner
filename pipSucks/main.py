a1b59e396ead4dd691024ef9aac39bb3ff6d5c7b
Benji Wright <3194212+benjiwright@users.noreply.github.com>
2024-02-18 08:32:16 -0700

    build on hello world

--- pipSucks/main.py
+++ pipSucks/main.py
@@ -1,24 +1,22 @@
-# This is a sample Python script.
-
-# Press Ctrl+F5 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-import tensorflow as tf
 import numpy as np
 
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense
 
-# defining a single layer of type Seq => Dense (fully connected)
-model = Sequential([Dense(units=1, input_shape=[1])])
+# layer to hold what is learned
+learned = Dense(units=1, input_shape=[1])
+# defining a single layer of type Seq => type Dense (fully connected)
+model = Sequential(learned)
 # optimizer = stochastic gradient descent, predicting how accurate guess was
 model.compile(optimizer='sgd', loss='mean_squared_error')
 
-# y=2x-1
+# y=2x-1 where weight is 2 and bias is -1
 xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
 ys = np.array([-3.0, -1.0, 1.0, 3.0, 5.0, 7.0], dtype=float)
 
-# learning step
+# learning x500 times
 model.fit(xs, ys, epochs=500)
 
 print(model.predict([10.0]))
+# relationships between X & Y
+print("Here is what I learned: {}".format(learned.get_weights()))
